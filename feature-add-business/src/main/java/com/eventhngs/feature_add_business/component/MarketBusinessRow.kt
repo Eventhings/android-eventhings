912f9df56d5c648962abd4cbb1fe021f4204222f
abdulhafizramadan <abdulhafizramadan.dev@gmail.com>
2023-12-07 16:09:22 +0700

    feat(ui-add-business): Make add business screen

--- feature-add-business/src/main/java/com/eventhngs/feature_add_business/component/MarketBusinessRow.kt
+++ feature-add-business/src/main/java/com/eventhngs/feature_add_business/component/MarketBusinessRow.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.magnifier
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
