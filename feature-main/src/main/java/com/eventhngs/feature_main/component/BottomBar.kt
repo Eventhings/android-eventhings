ff5786969ec178b738414d97fddcfe10eb776b2b
abdulhafizramadan <abdulhafizramadan.dev@gmail.com>
2023-12-16 17:05:20 +0700

    refactor(ui-bottom-bar): Refactor bottom bar

--- feature-main/src/main/java/com/eventhngs/feature_main/component/BottomBar.kt
+++ feature-main/src/main/java/com/eventhngs/feature_main/component/BottomBar.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
@@ -14,6 +15,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -84,7 +86,9 @@ fun BottomBar(
             }
             Image(
                 painter = painterResource(id = icon),
-                contentDescription = home.title
+                contentDescription = home.title,
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.size(26.dp)
             )
         }
         IconButton(
--- feature-profile/src/main/java/com/eventhngs/feature_profile/domain/ProfileMenu.kt
+++ feature-profile/src/main/java/com/eventhngs/feature_profile/domain/ProfileMenu.kt
@@ -1,7 +1,6 @@
 package com.eventhngs.feature_profile.domain
 
 import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
 
 data class ProfileMenu(
     @DrawableRes
